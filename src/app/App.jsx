d8d8af92512deebc2d08ab2443767faae86d3895
BT992 <berkturan992@gmail.com>
2026-02-01 23:49:44 +0300

    third

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Form from "../components/Form";
+import Footer from "../components/Footer";
+
+export default function App() {
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Form from "@/components/Form";
+import Footer from "@/components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  )
+}
+export default Home
